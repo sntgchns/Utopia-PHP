6e89c7bbe9a9524a56c7bf280b42d340998fe72d
Santiago Soñora <86539574+sntgchns@users.noreply.github.com>
2022-01-05 05:39:16 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'santiagochinas@hotmail.com';
+$asunto = 'Contacto desde UTOPIA';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
