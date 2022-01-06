25808ad1287515014ac4fd6eadda10df09c3a02b
Santiago Soñora <86539574+sntgchns@users.noreply.github.com>
2022-01-06 13:37:28 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,22 +1,41 @@
 <?php
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+require('PHPMailer/PHPMailerAutoload.php');
 
-$header = 'From: ' . $email . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$mail = new PHPMailer;
 
-$mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
-$mensaje .= "Su e-mail es: " . $email . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el: " . date('d/m/Y', time());
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$para = 'santiagosonora@gmail.com';
-$asunto = 'Contacto desde UTOPIA';
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.ionos.es';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'info@utopiansworld.com';                 // SMTP username
+$mail->Password = 'O[sga]ANI77INA';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+$mail->CharSet = 'UTF-8';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+$mail->From = $_POST['email'];
+$mail->FromName = $_POST['nombre'];
+$mail->addBCC('santiagosonora@gmail.com', 'Santiago Soñora');     // Add a recipient
+// $mail->addAddress('ellen@example.com');               // Name is optional
+// $mail->addReplyTo('info@example.com', 'Information');
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com');
 
-header("Location:https://utopiarea.com/");
-?>
+// $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+// $mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Contact from UTOPIA';
+// $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->addAddress('info@utopiansworld.com', "Utopian's World");
+$mail->Body    = $_POST['mensaje'];
+// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    header("Location:https://utopiansworld.com/");
+}
